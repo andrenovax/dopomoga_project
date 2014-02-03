8bfe7485926c5d4979f93d92cc8fe0a5595385a5
Andrey Novak <andrenovax@gmail.com>
2014-02-03 14:28:53 +0200

    settings

--- dopomoga_project/settings.py
+++ dopomoga_project/settings.py
@@ -78,7 +78,7 @@ USE_TZ = True
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -87,12 +87,11 @@ ALLOWED_HOSTS = ['*']
 
 TEMPLATE_DIRS = (TEMPLATE_PATH,)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (STATIC_PATH, )
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )#heroku configuratio
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )#heroku configuratio
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
