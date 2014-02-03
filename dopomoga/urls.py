7e27d280d759d725519d991483c40f9bea881e69
Andrey Novak <andrenovax@gmail.com>
2014-02-03 12:37:07 +0200

    movefilesup

---
+++ dopomoga/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import patterns, url
+from dopomoga import views
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'),
+        url(r'^about$', views.about, name='about'),
+        url(r'^project_inneed_all/$', views.project_inneed_all, name='project_inneed_all'),
+        url(r'^project_helper_all/$', views.project_helper_all, name='project_helper_all'),
+        url(r'^userinneed_all/$', views.userinneed_all, name='userinneed_all'),
+        url(r'^userhelper_all/$', views.userhelper_all, name='userhelper_all'),
+        url(r'^resource_all/$', views.resource_all, name='resource_all'),
+        url(r'^cause_all/$', views.cause_all, name='cause_all'),
+        url(r'^project_helper_all/(?P<project_name_url>\w+)/$', views.project_helper_item, name='project_helper_item'),
+        url(r'^project_inneed_all/(?P<project_name_url>\w+)/$', views.project_inneed_item, name='project_inneed_item'),
+        url(r'^userhelper_all/(?P<user_name_url>\w+)/$', views.user_helper_item, name='user_helper_item'),
+        url(r'^userinneed_all/(?P<user_name_url>\w+)/$', views.user_inneed_item, name='user_inneed_item'),
+        url(r'^resource_all/(?P<resource_name_url>\w+)/$', views.resource_item, name='resource_item'),
+        url(r'^cause_all/(?P<cause_name_url>\w+)/$', views.cause_item, name='cause_item'),
+        url(r'^register/$', views.register, name='register'),
+        url(r'^login/$', views.user_login, name='user_login'),
+        url(r'^logout/$', views.user_logout, name='user_logout'),
+        url(r'^get_UserInneed/$', views.get_UserInneed, name='get_UserInneed'),
+        url(r'^get_Resource/$', views.get_Resource, name='get_Resource'),
+        url(r'^get_Cause/$', views.get_Cause, name='get_Cause'),
+        url(r'^support_project/$', views.support_project, name='support_project'),
+)
---
+++ static/js/lists.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	$('.readmore+p, .cancel').hide();
+
+	/*===SUPPORT BUTTONS===*/
+    $('.support').click(function(){
+	    var itemid;
+	    itemid = $(this).attr("data-itemid");
+	    $.get('/dopomoga/support_project/', {itemid: itemid}, function(){});
+        $(this).prev().show();
+        $(this).next().show();
+        $(this).hide();
+	});
+    $('.resupport, .hsupport').click(function(){
+        $(this).prev().show();
+        $(this).next().show();
+        $(this).hide();
+	});
+    $('.cancel').click(function(){
+        $(this).next().show();
+        $(this).next().next().hide();
+        $(this).hide();
+	});
+});
